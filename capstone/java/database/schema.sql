1b9923ac8259818a8342f99151ab0279091bdd94
Eric Mangin <ericpaulmangin@gmail.com>
2022-09-27 08:18:14 -0700

    updating local changes from my branch

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -9,5 +9,27 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+-- Create Table Games
+CREATE TABLE games (
+	game_id SERIAL,
+	organizer_id int NOT NULL,
+	start_date date NOT NULL,
+	end_date date NOT NULL,
+	game_name varchar NOT NULL,
+	game_results varchar,
+	CONSTRAINT PK_games PRIMARY KEY (game_id),
+	CONSTRAINT FK_games FOREIGN KEY (organizer_id) REFERENCES users(user_id)
+);
+-- Create Table game_results
+CREATE TABLE game_results (
+	user_id int NOT NULL,
+	game_id int NOT NULL,
+	-- spot saved for (realtime / all-time results columns)
+	CONSTRAINT PK_game_results PRIMARY KEY(user_id, game_id),
+	CONSTRAINT FK_game_results_from_users FOREIGN KEY(user_id) REFERENCES users(user_id),
+	CONSTRAINT FK_game_results_from_games FOREIGN KEY(game_id) REFERENCES games(game_id)
+);
+
+
 
 COMMIT TRANSACTION;
