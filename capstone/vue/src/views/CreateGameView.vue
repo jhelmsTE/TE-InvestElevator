e397fadd5dcceed5c9c13a4f0e5b92de95079575
Cody Benway <cody.r.benway@gmail.com>
2022-09-28 12:50:42 -0400

    Updates to CreateGameView and java AuthenticationController

--- capstone/vue/src/views/CreateGameView.vue
+++ capstone/vue/src/views/CreateGameView.vue
@@ -1,70 +1,82 @@
 <template>
-<div>
+  <div>
     <h1>Welcome</h1>
-    <h2 id='invite'>Invite Players</h2>
-   
-    <tr>
-    <td v-for="user in users" v-bind:key="user.id" input type="checkbox">
-        <p>{{user.username}}</p>  
-    </td>
-    </tr>
-    
+    <h2 id="invite">Invite Players</h2>
+    <div class="checkBoxForm" v-for="user in users" v-bind:key="user.id">
+      <input class="checkboxes" type="checkbox">
+      <label for="user.id">{{ user.username }}</label>
+    </div>
+
     <h3 class="start-date">Start Date</h3>
-     <input type="date" id='start-date'>
+    <input type="date" id="start-date" />
 
     <h3 class="end-date">End date</h3>
-    <input type="date" id='end-date'>
+    <input type="date" id="end-date" />
 
-       <br>
+    <br />
     <button type="submit" class="submit">Create Game</button>
-    
-</div>
+  </div>
 </template>
 
 <script>
-import AuthService from '../services/AuthService.js'
+import AuthService from "../services/AuthService";
+
 export default {
   data() {
     return {
-      users: "",
+      users: [],
+      user: {
+        name: "",
+      },
     };
   },
   created() {
-    AuthService.getUsers().then(response => {
+    AuthService.getUsers().then((response) => {
       this.users = response.data;
     });
-  }
+  },
 };
-
 </script>
 
 <style>
-#invite{
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    text-align: left;
-    margin-left: 10px;
+#invite {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px honeydew;
+  text-align: left;
+  margin-left: 10px;
 }
 
-.start-date{
-justify-content: left;
-margin-left: 10px;
-}
-#start-date{
-justify-content: left;
-margin-left: 10px;
-}
-
-.end-date{
-justify-content: left;
-margin-left: 10px;
-}
-#end-date{
-margin-left: 10px;
-}
-.submit{
- margin-top: 20px;
- margin-left: 15px;
+.checkBoxForm {
+  padding-left: 10px;
+  padding-bottom: 20px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px honeydew;
 }
 
+.checkboxes {
+  margin-right: 10px;
+}
 
+.start-date {
+  justify-content: left;
+  margin-left: 10px;
+}
+#start-date {
+  justify-content: left;
+  margin-left: 10px;
+}
+
+.end-date {
+  justify-content: left;
+  margin-left: 10px;
+}
+#end-date {
+  margin-left: 10px;
+}
+.submit {
+  margin-top: 20px;
+  margin-left: 15px;
+}
 </style>
