1a0287252fce7d915704e7d26e1999120561d54f
Cody Benway <cody.r.benway@gmail.com>
2022-09-29 19:23:07 -0400

    Updates to the front-end buy/sell stocks

--- capstone/vue/src/views/BuyStocks.vue
+++ capstone/vue/src/views/BuyStocks.vue
@@ -1,25 +1,67 @@
 <template>
   <div>
-    <router-link class="return" :to="{ name: 'game-details' }">Back to Game Details</router-link>
+    <router-link class="return" :to="{ name: 'game-details' }"
+      >Back to Game Details</router-link
+    >
     <h1>Buy Stocks Here!</h1>
     <div class="buyStocks">
-      <p>Select stock you wish to purchase:</p>
-      <select class="search dropdown" v-model="selectedStock">
+      <p>Select stock you wish to purchase: {{ selectedCompany }}</p>
+      <select v-model="selectedCompany">
+        <option value="" disabled selected hidden>Select a company</option>
+        <option value="AAPL">Apple</option>
+        <option value="MSFT">Microsoft</option>
+        <option value="GOOG">Alphabet (Google)</option>
+        <option value="AMZN">Amazon</option>
+        <option value="TSLA">Tesla</option>
+        <option value="BRK-B">Berkshire Hathaway</option>
+        <option value="UNH">UnitedHealth</option>
+        <option value="JNJ">Johnson & Johnson</option>
+        <option value="V">Visa</option>
+        <option value="XOM">Exxon Mobil</option>
+        <option value="META">Meta Platforms (Facebook)</option>
+        <option value="WMT">Walmart</option>
+        <option value="LLY">Eli Lilly</option>
+        <option value="JPM">JPMorgan & Chase</option>
+        <option value="PG">Procter & Gamble</option>
+        <option value="NVDA">NVIDIA</option>
+        <option value="HD">Home Depot</option>
+        <option value="CVX">Chevron</option>
+        <option value="MA">Mastercard</option>
+        <option value="ABBV">AbbVie</option>
+        <option value="PFE">Pfizer</option>
+        <option value="BAC">Bank of America</option>
+        <option value="KO">Coca-Cola</option>
+        <option value="PEP">Pepsico</option>
+        <option value="MRK">Merck</option>
+        <option value="COST">Costco</option>
+        <option value="TMO">Thermo Fisher Scientific</option>
+        <option value="DHR">Danaher</option>
+        <option value="AVGO">Broadcom</option>
+        <option value="DIS">Walt Disney</option>
+      </select>
+      <!-- Save for later, just in case
+        <select class="search dropdown" v-model="selectedStock">
         <option v-for="stock in stocks" v-bind:key="stock.id">
           {{ stock.name }}
         </option>
-      </select>
+      </select> -->
       <div class="stockNumber">
-        <p>How many shares would you like to buy?</p>
-        <input v-model.number="number" placeholder="Insert number" />
+        <p>
+        <label for="quantity">How many shares would you like to buy? (Whole numbers only; no fractional shares allowed)</label>
+        </p>
+        <input type="number" id="quantity" name="quantity" min="1" max="999999999" placeholder="Insert number" />
       </div>
     </div>
     <div class="buttonContainer">
       <div>
-        <router-link class="button" :to="{ name: 'game-details' }">Confirm</router-link>
+        <router-link class="button" :to="{ name: 'game-details' }"
+          >Confirm</router-link
+        >
       </div>
       <div>
-        <router-link class="button" :to="{ name: 'game-details' }">Cancel Transaction</router-link>
+        <router-link class="button" :to="{ name: 'game-details' }"
+          >Cancel Transaction</router-link
+        >
       </div>
     </div>
   </div>
@@ -31,11 +73,12 @@ export default {
     return {
       number: "",
       selectedStock: "",
+      selectedCompany: "",
       stocks: [
-        { id: 1, name: "Apple" },
-        { id: 2, name: "Amazon" },
-        { id: 3, name: "Meta" },
-        { id: 4, name: "Google" },
+        // { id: 1, name: "Apple" },
+        // { id: 2, name: "Amazon" },
+        // { id: 3, name: "Meta" },
+        // { id: 4, name: "Google" },
       ],
       search: "",
     };
@@ -61,9 +104,9 @@ export default {
   display: flex;
   flex-direction: column;
   padding-left: 20%;
-  width: 20%;
+  width: 25%;
 }
-.buttonContainer{
+.buttonContainer {
   display: flex;
   padding-left: 20%;
 }
