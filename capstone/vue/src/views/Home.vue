598f092082b8dc93fc7e38fc6d61a552ed0d5ba1
Cody Benway <cody.r.benway@gmail.com>
2022-09-30 16:40:21 -0400

    Updates to front-end router, Home, and created JoinGame

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,58 +1,53 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <div id="welcome">
-      Welcome!
+    <div id="welcome">Welcome!</div>
+    <div class="user" v-for="user in $store.state" v-bind:key="user.id">
+      <p>{{ user.username }}</p>
     </div>
-    <div class= "user" v-for="user in $store.state" v-bind:key="user.id">
-      <p>{{user.username}}</p>
-    </div>
-    <div >
-  <router-link :to="{name: 'game-view'}" class="create-game"  >
-    Create Game
-  </router-link>
+    <router-link :to="{ name: 'game-view' }" class="create-game">
+      Create Game
+    </router-link>
+    <br />
+    <router-link :to="{ name: 'viewAllGames' }" class="current-games">
+      View all Current Games
+    </router-link>
+    <br />
+    <router-link :to="{ name: 'joinGame' }" class="join-game">
+      Join a Game
+    </router-link>
   </div>
-  <br>
-  <router-link :to="{name: 'viewAllGames'}" class="current-games">
-    View all Current Games
-  </router-link>
-
-
-  </div>
-
 </template>
 
 <script>
-import GameService from "../services/GameService"
-
+import GameService from "../services/GameService";
 
 export default {
   name: "home",
   data() {
     return {
       user: {
-        username:"",
-      }
-    }
-  }, 
-  methods:{
-    createGame(){
-      this.game.username = this.$store.state.user.username
+        username: "",
+      },
+    };
+  },
+  methods: {
+    createGame() {
+      this.game.username = this.$store.state.user.username;
       GameService.create(this.game);
-    }
-  }
-
+    },
+  },
 };
 </script>
 
 <style>
-
-.home{
+.home {
   display: flex;
-  flex-direction:column;
+  flex-direction: column;
   justify-content: center;
 }
-body, html{
+body,
+html {
   background: rgb(26, 17, 89);
   color: white;
   margin: 0px;
@@ -60,32 +55,37 @@ body, html{
   width: 100%;
   min-height: 100%;
 }
-#welcome{
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+#welcome {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   font-weight: bold;
   margin-left: 10px;
 }
-.current-games{
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  font-weight: bold;
-  margin-left: 10px;
-}
-
-.create-game{
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+.current-games {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   font-weight: bold;
   margin-left: 10px;
 }
 
-.user{
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+.create-game {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  margin-left: 10px;
+}
+
+.join-game{
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  margin-left: 10px;
+}
+
+.user {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   font-weight: bold;
   margin-left: 10px;
   font-size: large;
 }
 
-a{
- color: white; 
+a {
+  color: white;
 }
-
 </style>
---
+++ capstone/vue/src/views/JoinGame.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class>
+    <h1>Welcome to the Game Board!</h1>
+    <h3>Here are the games you can join! Click a game to join</h3>
+    <div class = "all-cards">
+    <router-link :to="{name: 'game-details', params: {id: game.id}}" class="card" v-for="game in games" v-bind:key="game.id">
+      <h4>{{ game.gameName }}</h4>
+    </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+ import gameService from '../services/GameService';
+
+export default {
+  data() {
+    return {
+      games: [],
+      game: {
+        name: "",
+      },
+    };
+  },
+   created() {
+     gameService.getAllGames().then(response => {
+       this.games = response.data;
+     })
+   }
+};
+</script>
+
+<style>
+h1,
+h3 {
+  display: flex;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px black;
+  padding-right: 12px;
+  justify-content: center;
+}
+a:link {
+  justify-content: center;
+  align-items: center;
+}
+.all-cards{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  border: 2px solid black;
+  border-radius: 10px;
+  width: 250px;
+  height: 200px;
+  margin: 40px;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px black;
+  text-decoration: none;
+  padding-right: 12px;
+  background-color: #44c767;
+  color: white;
+}
+</style>
