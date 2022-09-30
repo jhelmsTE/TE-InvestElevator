766bc828313bf775c067c7b7d4c49b4022bab60f
Cody Benway <cody.r.benway@gmail.com>
2022-09-30 18:51:56 -0400

    Created search bar for view games and join games on front-end

--- capstone/vue/src/views/ViewAllGames.vue
+++ capstone/vue/src/views/ViewAllGames.vue
@@ -2,31 +2,51 @@
   <div class>
     <h1>Welcome to Your Game Board!</h1>
     <h3>Here are the games you've joined</h3>
-    <div class = "all-cards">
-    <router-link :to="{name: 'game-details', params: {id: game.id}}" class="card" v-for="game in games" v-bind:key="game.id">
-      <h4>{{ game.gameName }}</h4>
-    </router-link>
+    <div class="searchBar">
+      <input type="text" v-model="selectedGame" placeholder="Search games..." />
+    </div>
+    <div class="all-cards">
+      <router-link
+        :to="{ name: 'game-details', params: { id: game.id } }"
+        class="card"
+        v-for="game in filteredGames"
+        v-bind:key="game.id"
+      >
+        <h4>{{ game.gameName }}</h4>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
- import gameService from '../services/GameService';
+import gameService from "../services/GameService";
 
 export default {
   data() {
     return {
+      selectedGame: "",
       games: [],
       game: {
         name: "",
       },
     };
   },
-   created() {
-     gameService.getAllGames().then(response => {
-       this.games = response.data;
-     })
-   }
+  created() {
+    gameService.getAllGames().then((response) => {
+      this.games = response.data;
+    });
+  },
+  computed: {
+    filteredGames() {
+      return this.games.filter((game) => {
+        return (
+          game.gameName
+            .toLowerCase()
+            .indexOf(this.selectedGame.toLowerCase()) != -1
+        );
+      });
+    },
+  },
 };
 </script>
 
@@ -44,7 +64,7 @@ a:link {
   justify-content: center;
   align-items: center;
 }
-.all-cards{
+.all-cards {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -66,5 +86,10 @@ a:link {
   padding-right: 12px;
   background-color: #44c767;
   color: white;
+}
+.searchBar {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 }
 </style>
